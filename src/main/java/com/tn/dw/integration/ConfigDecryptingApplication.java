a6894786817ff996bfae0aa6084b9068f824f0b6
tnagelkerke <tom.nagelkerke@meltwater.com>
2017-10-03 23:05:17 +0200

    getting decryption config from extensible config class

--- src/main/java/com/tn/dw/integration/ConfigDecryptingApplication.java
+++ src/main/java/com/tn/dw/integration/ConfigDecryptingApplication.java
@@ -1,18 +1,12 @@
 package com.tn.dw.integration;
 
-import com.tn.dw.EncryptionClient;
 import io.dropwizard.Application;
-import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 
-import java.util.function.Function;
-
-public abstract class ConfigDecryptingApplication<T extends Configuration> extends Application<T> {
+public abstract class ConfigDecryptingApplication<T extends ConfigDecryptingConfiguration> extends Application<T> {
     @Override
     public void initialize(Bootstrap<T> bootstrap) {
         super.initialize(bootstrap);
-        bootstrap.addBundle(new DecryptionConfiguredBundle<>(getEncryptionClient()));
+        bootstrap.addBundle(new DecryptionConfiguredBundle<>(ConfigDecryptingConfiguration::getKeyEncryptionClient));
     }
-
-    abstract Function<T, EncryptionClient> getEncryptionClient();
 }
